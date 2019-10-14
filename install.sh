668e0914eb91860ff47e1c4514af6ef5578b1098
tillbaks <johannes@bluml.se>
2019-10-14 17:03:00 +0200

    install script for root user

--- install.sh
+++ install.sh
@@ -7,42 +7,57 @@
 # yay is required to install packages
 ! type yay &> /dev/null && echo "yay is required. Please install it to continue." && exit 1
 
-requiredPackages="
-lf
+requiredPackages="lf
 neovim
-zsh zsh-autosuggestions zsh-completions zsh-syntax-highlighting
-fzf fzy
+zsh
+zsh-autosuggestions
+zsh-completions
+zsh-syntax-highlighting
+fzf
+fzy
 the_silver_searcher
-gitflow-avh gitflow-zshcompletion-avh
-ccls
-"
+gitflow-avh
+gitflow-zshcompletion-avh
+ccls"
 
-guiPackages="
-numix-icon-theme
+guiPackages="numix-icon-theme
 redshift-wlr-gamma-control-git
 ttf-google-fonts-typewolf
-sway rofi termite
-imv mpv evince
-firefox-developer-edition brave-bin
-transmission-remote-gtk
-"
+sway
+rofi
+termite
+imv
+mpv
+evince
+firefox-developer-edition
+brave-bin
+transmission-remote-gtk"
 
-gamePackages="
-retroarch libretro-fceumm-git \
-nottetris2 \
-antimicro obs-studio \
-moonlight-qt
-"
+gamePackages="retroarch
+libretro-fceumm-git
+nottetris2
+antimicro
+obs-studio
+moonlight-qt"
 
-echo "=> Installing required packages"
+# Start installing
 
-yay -S --needed $requiredPackages
 if [ -d "$HOME/.config/zsh/.zsh-nvm" ]; then
   echo "zsh-nvm exists - updating"
   git -C "$HOME/.config/zsh/.zsh-nvm" pull
 else
   git clone https://github.com/lukechilds/zsh-nvm.git "$HOME/.config/zsh/.zsh-nvm"
 fi
+
+# Nothing else should be installed by root users
+if [ $USER == "root" ]; then
+  echo "=> Done."
+  exit 0
+fi
+
+echo "=> Installing required packages"
+yay -S --needed $requiredPackages
+
 echo "=> Do you want to install GUI packages? [Y/N]"
 read installGui
 case $installGui in
